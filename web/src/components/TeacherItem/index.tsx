bff0551d5cd40bb98c584187084e615b0ae70f2a
Jonatas Ferreira <ferreiradelimajonatas@gmail.com>
2020-08-05 13:08:00 -0300

    finalização do desktop

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,33 +3,52 @@ import React from 'react'
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
 
 import './styles.css'
+import api from '../../services/api'
 
-const TeacherItem = () => {
+export interface Teacher {
+  id: number,
+  avatar: string,
+  bio: string,
+  cost: number,
+  name: string,
+  subject: string,
+  whatsapp: string
+}
+
+interface TeacherItemProps {
+  teacher: Teacher
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post('connections', {
+      user_id: teacher.id
+    })
+  }
+  
   return (
     <article className="teacher-item">
     <header>
-      <img src="https://avatars0.githubusercontent.com/u/62180037?s=460&u=f6a787a7f4dbbb48198edd09555eafdf3ae0680a&v=4" alt="jonatas"/>
+      <img src={teacher.avatar} alt={teacher.name}/>
       <div>
-        <strong>Jonatas Ferreira</strong>
-        <span>Química</span>
+        <strong>{teacher.name}</strong>
+        <span>{teacher.subject}</span>
       </div>
     </header>
 
     <p>
-      Entusiasta das melhores tecnologias de química avançada.
-      <br/><br/>
-      Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências.
+      {teacher.bio}
     </p>
 
     <footer>
       <p>
         Preço/hora
-        <strong>R$ 40,00</strong>
+        <strong>R$ {teacher.cost}</strong>
       </p>
-      <button type="button">
+      <a onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`} target="blank">
         <img src={whatsappIcon} alt="Whatsapp"/>
         Entrar em contato
-      </button>
+      </a>
     </footer>
   </article>
   )
